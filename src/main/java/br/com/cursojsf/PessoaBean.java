afa37fef5aded709c4d625fc4ef579f18971f0f4
Nando <fernando.andrade.inf@gmail.com>
2019-08-22 23:49:19 -0300

    falando sobre escopos

--- src/main/java/br/com/cursojsf/PessoaBean.java
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -1,26 +1,42 @@
 package br.com.cursojsf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 
 @ManagedBean(name = "pessoaBean")
+@ViewScoped
 public class PessoaBean {
 	
 	private String nome;
-	private String sobrenome;
+	
+	private List<String> nomes = new ArrayList<>();
+	
+	public String addNome() {
+		nomes.add(nome);
+		return "";
+	}
+		
+	public List<String> getNomes() {
+		return nomes;
+	}
+
+	public void setNomes(List<String> nomes) {
+		this.nomes = nomes;
+	}
 	
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public String getSobrenome() {
-		return sobrenome;
-	}
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
 	
+
 	
 	
 	
